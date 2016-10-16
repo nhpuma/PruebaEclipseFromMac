56a87d0837f7ef757c2333367f104c69e89e11b7
nielsenpuma <nielsenpumajihuallanca@192.168.1.46>
2016-10-16 18:56:56 -0500

    Trabajando desde MAC.

---
+++ PruebaGitDesdeMac/src/main/SumaNumeros.java
@@ -0,0 +1,18 @@
+package main;
+
+public class SumaNumeros {
+	private int num1, num2;
+
+	public SumaNumeros() {
+	}
+
+	public SumaNumeros(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int sumar(){
+		return num1+num2;
+	}
+
+}
