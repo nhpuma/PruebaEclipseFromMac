a51b1ff3ff2232fe4e256944d5130eb897789af4
NielsenRichard <nielhpuma@outlook.com>
2016-10-16 17:04:38 -0500

    Cambios windows

--- PruebaGitDesdeMac/src/main/Principal.java
+++ PruebaGitDesdeMac/src/main/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Saludos desde MAC");
 		//Desde windows
-		System.out.println("Saludos desde WINDOWS");
+		System.out.println("Saludos desde WINDOWS:");
 		//Fin windows
 
 	}
