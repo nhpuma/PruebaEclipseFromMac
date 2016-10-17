9b8095c335023ffd25ecc27babe53707434f4ea6
nielsenpuma <nielsenpumajihuallanca@192.168.1.46>
2016-10-16 19:18:21 -0500

    Agregando un formulario sin configurar Eclipse

---
+++ PruebaGitDesdeMac/src/gui/Principal.java
@@ -0,0 +1,63 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setTitle("Formulario Principal");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//El evento no se puede llamar
+			}
+		});
+		btnMostrar.setBounds(327, 6, 117, 29);
+		contentPane.add(btnMostrar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(20, 6, 295, 253);
+		contentPane.add(scrollPane);
+		
+		JTextArea txtS = new JTextArea();
+		scrollPane.setViewportView(txtS);
+	}
+}
